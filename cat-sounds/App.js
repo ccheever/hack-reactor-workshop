290f2da6c4a52b929be2c994cabb4b2d5953a7d4
Charlie Cheever <ccheever@gmail.com>
2017-08-09 14:11:42 -0700

    One video playing compnent

--- cat-sounds/App.js
+++ cat-sounds/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import Expo from "expo";
+
+const { Audio, Video } = Expo;
 
 export default class App extends React.Component {
+  componentDidMount() {
+    Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX, // TODO(Abi): Switch back to INTERRUPTION_MODE_IOS_DO_NOT_MIX
+      playsInSilentModeIOS: true, // config.muteVideo ? false : true,
+      shouldDuckAndroid: true, // TODO(Abi): Is this the common behavior on Android?
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <Expo.Video
+          source={require("./assets/clips/1.mp4")}
+          rate={1.0}
+          volume={1.0}
+          muted={false}
+          resizeMode="cover"
+          shouldPlay
+          isLooping={false}
+          style={{ width: 300, height: 300 }}
+        />
+
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
@@ -14,8 +39,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
